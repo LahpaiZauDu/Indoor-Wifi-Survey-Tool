53ef0f0c4352b45713989219fef3cf511d02200c
Matthew <lahpai_zaudu@cmu.ac.th>
2023-01-17 22:11:35 +0700

    append rssi and xy coordinates

--- Functions.py
+++ Functions.py
@@ -1,10 +1,8 @@
 import subprocess
 import csv
-import cv2
-from PIL import ImageDraw
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from typing import Tuple
+import numpy as np
 
 
 class Toolbar(NavigationToolbar2Tk):
@@ -34,39 +32,11 @@ def make_csv(datas):
     # field names
     fields = ['SSID', 'BSSID', 'RSSI', 'CHANNEL', 'HT', 'CC', 'SECURITY']
     rows = datas
-    with open('./Data/Access_points.csv', 'w') as f:
+    with open('./Data/Home_Access_points.csv', 'w') as f:
         # using csv.writer method from CSV package
         write = csv.writer(f)
         write.writerow(fields)
         write.writerows(rows)
-
-
-def Capture_Event(event, x, y, flags, params):
-    # If the left mouse button is pressed
-    if event == cv2.EVENT_LBUTTONDOWN:
-        # clicked point of coordinates
-        print(f"({x}, {y})")
-
-
-def FilePath(filepath):
-    tostring = str(filepath)
-    img = cv2.imread(tostring, 1)
-    # Show the Image
-    cv2.imshow('image', img)
-    # Set the Mouse Callback function, and call
-    # the Capture_Event function.
-    cv2.setMouseCallback('image', Capture_Event)
-    # Press any key to exit
-    cv2.waitKey(0)
-    # Destroy all the windows
-    cv2.destroyAllWindows()
-
-
-def add_line(im, color):
-    draw = ImageDraw.Draw(im)
-    draw.line((0, 0) + im.size, fill=color, width=10)
-    draw.line((0, im.size[1], im.size[0], 0), fill=color, width=10)
-    return im
 
 
 def draw_figure_w_toolbar(canvas, fig, canvas_toolbar):
@@ -89,7 +59,28 @@ def class_changed_cb(new_class: str):
 
 def point_added_cb(position: Tuple[float, float], klass: str):
     x, y = position
-    print(f"New point of class {klass} added at {x=}, {y=}")
+    mylist = [x, y]
+    scan_cmd = subprocess.Popen(
+        ['sudo', 'airport', '-s'],    stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    scan_out, scan_err = scan_cmd.communicate()
+    scan_out_data = []
+    scan_out_lines = str(scan_out).split("\\n")[1:-1]
+    for each_line in scan_out_lines:
+        split_line = [e for e in each_line.split(" ") if e != ""]
+        while len(split_line) > 7:
+            new_value = split_line[0] + split_line[1]
+            del split_line[0]
+            split_line[0] = new_value
+        scan_out_data.append(split_line + mylist)
+
+    fields = ['SSID', 'BSSID', 'RSSI', 'CHANNEL',
+              'HT', 'CC', 'SECURITY', 'x', 'Y']
+    rows = scan_out_data
+    with open('./Data/Home_Access_points.csv', 'a') as f:
+        # using csv.writer method from CSV package
+        write = csv.writer(f)
+        write.writerow(fields)
+        write.writerows(rows)
 
 
 def point_removed_cb(position: Tuple[float, float], klass: str, idx):
